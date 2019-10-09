19937e8cee35fe7b9da241ac9a236001f4f62406
Nicolas Mohr <Nico.Mohr@gmx.net>
2019-10-09 13:56:11 +0200

    basic express server

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello world")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello from express!'))
+
+app.listen(port, () => console.log(`Listening on port ${port}...`))
